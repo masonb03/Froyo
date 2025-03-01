8c467de994f79bf014906632160dcccb48e93929
Mason <bevinsm355@gmail.com>
2025-03-01 18:47:07 -0500

    created js file and created a prompt for the user

---
+++ index.js
@@ -0,0 +1,9 @@
+// Step 1: Create a prompt for the user to input their flavor of choice.
+// Step 2: Split the users strings into arrays.
+// Step 3: Create a loop to itierate through the flavors.
+// Step 4: Use an object to keep track of how many flavors their are.
+// Step 4: Update the object after the input has been confirmed
+
+let userInput = prompt("Enter a list of what flavors you want>",
+    vanilla,vanilla,vanilla,strawberry,coffee
+);
